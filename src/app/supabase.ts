615021de8d16396aa4153d15549c94fd856c6c48
Edgar Ortiz <estadia_uttab@outlook.com>
2026-04-20 23:57:27 -0600

    Agregar servicio Supabase y pruebas unitarias; actualizar dependencias en package.json y package-lock.json

---
+++ src/app/supabase.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  private supabaseUrl = 'https://cqsxhzsjtkvjvpyrmoen.supabase.co/rest/v1/';
+  private supabaseKey = 'sb_publishable_uKyGMDn03LqOT3DZU_E4Vw_RgKK7dj3';
+
+  constructor() {
+    this.supabase = createClient(this.supabaseUrl, this.supabaseKey);
+  }
+
+  async obtenerFlores() {
+    const { data, error } = await this.supabase
+      .from('flores')
+      .select('*, Categoria_flores(nombre)');
+
+    if (error) {
+      console.error('Error al traer las flores:', error);
+      return [];
+    }
+    return data;
+  }
+}
